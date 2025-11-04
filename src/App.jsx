b234adca3b1873b843331e7dbcb41a237d97f724
SOLOxLEVELING <adnanshaikh1652000@gmail.com>
2025-11-04 16:10:42 +0530

    implement microsoft clarity

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // src/App.jsx
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Routes, Route, NavLink } from "react-router-dom";
 import GenerativeMeshCanvas from "./components/GenerativeMeshCanvas";
 import ContactModal from "./components/ContactModal";
